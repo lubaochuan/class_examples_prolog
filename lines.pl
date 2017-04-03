7cb226cb6ad79d3541be1eaaf30202ba732a8424
lubaochuan <lubaochuan@gmail.com>
2017-04-03 03:38:47 -0500

    add comments to lines.pl

--- lines.pl
+++ lines.pl
@@ -1,3 +1,8 @@
+% http://www.learnprolognow.org/lpnpage.php?pagetype=html&pageid=lpn-htmlse5
+% You can write useful programs simply by using complex terms to define
+% interesting concepts. Unification can then be used to pull out the
+% information you want.
+
 vertical(line(point(X,Y),point(X,Z))).
 horizontal(line(point(X,Y),point(Z,Y))).
 
