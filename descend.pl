ec2b4e343267b5f2dfe2336b29a1721314f465af
lubaochuan <lubaochuan@gmail.com>
2017-04-03 11:39:18 -0500

    add descend.pl eating.pl

---
+++ descend.pl
@@ -0,0 +1,17 @@
+% child(X,Y): Y is a child of X
+child(anne,bridget).
+child(bridget,caroline).
+child(caroline,donna).
+child(donna,emily).
+
+descend(X,Y):-
+  child(X,Y).
+
+descend(X,Y):-
+  child(X,Z),
+  descend(Z,Y).
+
+/** <examples> Your example queries go here, e.g.
+?- descend(anne,donna).
+?- descend(bridget,emily).
+*/
